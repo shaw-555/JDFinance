d003453088611bf3dc5f47b4a52864914599a747
shaw-555 <shaw18664590570@163.com>
2021-05-05 21:16:59 +0800

    quant element

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,10 @@
 import Vue from "vue"
 import App from "./App.vue"
 import router from "./router"
+import VueAwesomeSwiper from 'vue-awesome-swipre'
 
 Vue.config.productionTip = false
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
