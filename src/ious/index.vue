e1bb976e346c680312a9aac4cb11e37d128ca398
shaw-555 <shaw18664590570@163.com>
2021-05-12 21:55:53 +0800

    完成ious页面基础架构

---
+++ src/ious/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="ious">
+    <heador/>
+    <ious-slider/>
+    <ious-welfare/>
+    <ious-footer/>
+    <navbar/>
+  </div>
+</template>
+
+<script>
+import heador from '../public/heador'
+import navbar from '../public/navbar'
+import IousFooter from './footer.vue'
+import IousSlider from './Islider.vue'
+import IousWelfare from './welfare.vue'
+
+export default {
+  name: 'Ious',
+  components: {
+    heador,
+    navbar,
+    IousFooter,
+    IousSlider,
+    IousWelfare
+  }
+}
+</script>
---
+++ src/ious/welfare.vue
@@ -0,0 +1,50 @@
+<template>
+  <panel title="理财精选">
+    <section :class="$style.content">
+      <div class="item">
+        <img src="//img12.360buyimg.com/jrpmobile/jfs/t3670/58/543125556/2606/a65096b9/580d80afN63b1085a.png?width=76&height=76" alt="">
+        <div>
+            <h4>月度理财</h4>
+            <p>预期年化收益率4.50%</p>
+        </div>
+        <em>去赚钱 > </em>
+      </div>
+      <div class="item">
+        <img src="//img12.360buyimg.com/jrpmobile/jfs/t3679/44/511862277/2728/c1c6b2bf/580d8189N6b6f8052.png?width=76&height=76" alt="">
+        <div>
+            <h4>季度理财</h4>
+            <p>历史年化回报率5.50%</p>
+        </div>
+        <em>去赚钱 > </em>
+      </div>
+      <div  class="item">
+        <img src="//img12.360buyimg.com/jrpmobile/jfs/t3331/69/580199498/3074/d5bb21c5/580d7f6cN7f35e596.png?width=76&height=76" alt="">
+        <div>
+            <h4>一年理财</h4>
+            <p>历史年化回报率5.80%</p>
+        </div>
+        <em>去赚钱 > </em>
+      </div>
+    </section>
+  </panel>
+</template>
+
+<script>
+import panel from '../core/panel.vue'
+export default {
+  name: 'IousWelfare',
+  components: {
+    panel
+  }
+}
+</script>
+
+<style lang="scss" module>
+  @import '../assets/styles/element.scss';
+  .panel{
+    @include panel;
+    >h4{
+      border-bottom: 1px solid #ddd;
+    }
+  }
+</style>
